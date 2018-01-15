81ea0eeb116b61644f868cfb0ab5f8759e08c84d
hikboy <hikboy@163.com>
2018-01-15 15:51:34 +0800

    add the iterator type

--- iterator.cpp
+++ iterator.cpp
@@ -1,18 +1,24 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 //#include <container>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::vector;
 //using std::container;
 
 int main(void)
 {
     string s("some string");
-    
+    vector<int>::iterator it;
+    string::iterator it2;
+    vector<int>::const_iterator it3;
+    string::const_iterator it4;
+
     for(auto it = s.begin(); it != s.end() && !isspace(*it); ++it)
         *it = toupper(*it);
 
