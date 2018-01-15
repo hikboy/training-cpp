b3b43586aa6ec1e6adaf2155947d918465331212
hikboy <hikboy@163.com>
2018-01-15 20:00:41 +0800

    add new code for iterator.cpp

--- iterator.cpp
+++ iterator.cpp
@@ -31,5 +31,28 @@ int main(void)
 
     cout << s << endl;
 
+    vector<int> v;
+    const vector<int> cv;
+    auto t1 = v.begin();
+    auto t2 = cv.begin();
+    auto t3 = v.cbegin();
+
+    vector<string> text;
+
+    for(auto it = text.cbegin(); it != text.cend() && !it->empty(); ++it)
+        cout << *it << endl;
+
+    auto mid1 = text.begin() + text.size() / 2;
+
+    auto beg = text.begin(), end = text.end();
+    auto mid = text.begin() + (end - beg) / 2;
+    string sought = "A";
+    while(mid != end && *mid != sought){
+        if(sought < *mid)
+            mid = end;
+        else
+            beg = mid + 1;
+        mid = beg + (end - beg) / 2;
+    }
     return 0;
 }
